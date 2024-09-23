793c211f1f0d077c15f1403d63db1d614ed44aa1
qcmarcel <qc.marcel@gmail.com>
2024-09-23 15:26:38 -0500

    Initial commit
    initializing Spring Boot

---
+++ src/main/java/com/stefanini/technicaltest/StefaniniTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.stefanini.technicaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StefaniniTechnicalTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StefaniniTechnicalTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stefanini/technicaltest/StefaniniTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanini.technicaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StefaniniTechnicalTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
